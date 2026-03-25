e8623a58158d03dcca00409c4d78abf215461515
relsam-atinersier <github-personal.abacus946@passmail.net>
2026-03-25 18:48:33 -0500

    dark mode

--- src/elements/kiosk-app.ts
+++ src/elements/kiosk-app.ts
@@ -1,8 +1,18 @@
-import { LitElement, html } from 'lit'
+import { LitElement, html, css } from 'lit'
 import { customElement } from 'lit/decorators.js'
 
 @customElement('kiosk-app')
 export class KioskApp extends LitElement {
+  static styles = css`
+    :host {
+      display: block;
+      background: black;
+      color: #00bfff;
+      min-height: 100vh;
+      padding: 1rem;
+    }
+  `
+
   render() {
     return html`<div>hello, world</div>`
   }
